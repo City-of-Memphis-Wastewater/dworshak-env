86a38d052d1ad06ca82147909a1acb8a16baed2e
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-02-13 12:27:46 -0600

    placeholders in src/dworshak_env/dworshak_env.py

---
+++ src/dworshak_env/dworshak_env.py
@@ -0,0 +1,14 @@
+# placeholders
+
+class DworshakEnv:
+    def __init__(self,path):
+        self.path = path
+    def get():
+        pass
+    def set():
+        pass
+
+
+def dworshak_env():
+    DworshakEnv().get
+
