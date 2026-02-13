0668841caae10c72d930dd58afe0dc9c1607e4a5
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-02-13 12:33:15 -0600

    add __main__ to choose the proper cli.py

---
+++ src/dworshak_env/__main__.py
@@ -0,0 +1,10 @@
+# src/dworshak_env/__main__.py
+try:
+    from .cli import app
+    def run():
+        app()
+except ImportError:
+    from .cli_stdlib import main as run
+
+if __name__ == "__main__":
+    run()
