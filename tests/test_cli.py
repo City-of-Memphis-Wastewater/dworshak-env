e01e3c0a4285ab23361499e9695eabed3309f856
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-03-17 22:35:18 -0500

    establish pytest for pytest cli

---
+++ tests/test_cli.py
@@ -0,0 +1,42 @@
+# tests/test_cli.py
+"""
+How can I make a test optional, like if only a certain pyproject.toml option dependency group is used?
+"""
+import pytest
+
+typer = pytest.importorskip("typer")
+from typer.testing import CliRunner
+from dworshak_env.cli import app
+
+runner = CliRunner()
+
+@pytest.mark.typer
+def test_version():
+    cmd = ["--version"]
+    result = runner.invoke(app,cmd)
+    assert result.exit_code == 0
+
+@pytest.mark.typer
+def test_get():
+    cmd = ["get","milk"]
+    result = runner.invoke(app,cmd)
+    assert result.exit_code == 0
+
+@pytest.mark.typer
+def test_set():
+    cmd = ["set","milk","0"]
+    result = runner.invoke(app,cmd)
+    assert result.exit_code == 0
+
+@pytest.mark.typer
+def test_remove():
+    cmd = ["remove","milk"]
+    result = runner.invoke(app,cmd)
+    assert result.exit_code == 0
+
+@pytest.mark.typer
+def test_list():
+    cmd = ["list"]
+    result = runner.invoke(app,cmd)
+    assert result.exit_code == 0
+
