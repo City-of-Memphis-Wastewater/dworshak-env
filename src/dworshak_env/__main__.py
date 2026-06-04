b8e4fb1441c47a4976f1777bc652aae679638a55
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-06-03 22:46:20 -0500

    stdlib cli is out. simplify.

--- src/dworshak_env/__main__.py
+++ src/dworshak_env/__main__.py
@@ -1,10 +1,28 @@
 # src/dworshak_env/__main__.py
+"""
+Entry point for the dworshak-env Rich/Typer CLI.
+"""
+TYPERSUCCESS=None
 try:
+    import typer
+    TYPERSUCCESS=True
+except (ImportError, ModuleNotFoundError):
+    TYPERSUCCESS=False
+
+if TYPERSUCCESS:
+    # Attempt to use the feature-rich CLI
     from .cli import app
     def run():
         app()
-except ImportError:
-    from .cli_stdlib import main as run
-
+else:
+    import sys
+    def run():
+        print(
+            "Please install this package with the 'typer' extra to utilize the CLI.",
+            file=sys.stderr
+        )
+        sys.exit(1)
 if __name__ == "__main__":
     run()
+
+
