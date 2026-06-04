9a0058487c1d668b6a3016c0a42e3da2c4449081
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-06-04 00:05:37 -0500

    set up debug logging

--- README.md
+++ README.md
@@ -76,7 +76,7 @@ dworshak-env helptree
 ```
 
 <p align="center">
-  <img src="https://raw.githubusercontent.com/City-of-Memphis-Wastewater/dworshak-env/main/assets/dworshak-env_v0.1.6_helptree.svg" width="100%" alt="Screenshot of the dworshak-env CLI helptree">
+  <img src="https://raw.githubusercontent.com/City-of-Memphis-Wastewater/dworshak-env/main/assets/dworshak-env_v0.1.8_helptree.svg" width="100%" alt="Screenshot of the dworshak-env CLI helptree">
 </p>
 
 `helptree` is utility funtion for Typer CLIs, imported from the `typer-helptree` library.
---
+++ src/dworshak_env/logging_setup.py
@@ -0,0 +1,20 @@
+# src/dworshak_config/logging_setup.py
+from __future__ import annotations
+import logging
+import sys
+from rich.logging import RichHandler
+from rich.console import Console
+console = Console(stderr=True)
+
+def configure_root_logging_for_application(debug: bool):
+    INTENT="app"
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers[:]:
+        root_logger.removeHandler(handler)
+
+    level = logging.DEBUG if debug else logging.WARNING
+    root_logger.setLevel(level)
+    handler = RichHandler(console=console, show_time=debug, show_path=debug,log_time_format="[%H:%M:%S]")
+    handler.setFormatter(logging.Formatter("%(message)s"))
+    root_logger.addHandler(handler)
+    root_logger.debug(f"Debug logging enabled for {INTENT}.")
